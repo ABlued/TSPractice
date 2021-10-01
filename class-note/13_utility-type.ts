fa5a82ab706fb178f484359656b2f062ae923366
ABlued <53801395+ABlued@users.noreply.github.com>
2021-10-01 16:14:35 +0900

    유틸리티 타입 Omit, Parital 실습

--- class-note/13_utility-type.ts
+++ class-note/13_utility-type.ts
@@ -25,3 +25,17 @@ type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>
 function displayProductDetail(shoppingItem:ShoppingItem) {
     
 }
+
+// Omit Pick과 정반대의 개념이다. Pick은 자신이 사용하고 싶은 것만 골라내는 대신
+// Omit은 사용하고 싶지 않은 것을 골라내는 것이다.
+// 사용법 : Omit<T K extend keyof T> T:타입, K:타입 T에 들어있는 속성 K들 
+const Phone:Omit<Product, 'brand'| 'stock'> = {
+    id:3,
+    name:'갤럭시',
+    price:100000
+}
+
+// Parital은 특정 타입의 부분 집합을 만족하는 타입을 정의할 수 있다.
+const myProduct:Partial<Product>={};        // 아무것도 지정하지 않아도 된다.
+const yourProduct:Partial<Product>={id:1};      // 속성을 부분적으로 지정해도 되며
+const ourProduct:Partial<Product>={id:1, name:'clothes', price:30000, brand:'musinsa', stock:30};   // 모든 속성을 지정해도 된다.
