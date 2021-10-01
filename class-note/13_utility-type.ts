d342ffbccd341ae86d8b2541dde940ec9d2df896
ABlued <53801395+ABlued@users.noreply.github.com>
2021-10-01 16:29:41 +0900

    유틸리티 타입 Parital 구현

--- class-note/13_utility-type.ts
+++ class-note/13_utility-type.ts
@@ -6,7 +6,7 @@ interface Product {
     stock: number;
 }
 
-// 상품목록을 받아오기 위한 API 함수
+// 1. 상품목록을 받아오기 위한 API 함수
 function fetchProducts():Promise<Product[]> {
     
     return
@@ -18,7 +18,7 @@ interface ProductDetail{
     price: number;
 }
 
-// 상품 하나의 상세정보를 받아오기 위한 API 함수 
+// 2. 상품 하나의 상세정보를 받아오기 위한 API 함수 
 // 그래서 Pick이란 타입을 쓰기도 한다.
 // 사용법 : Pick<T K extend keyof T> T:타입, K:타입 T에 들어있는 속성 K들 
 type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>
@@ -39,3 +39,54 @@ const Phone:Omit<Product, 'brand'| 'stock'> = {
 const myProduct:Partial<Product>={};        // 아무것도 지정하지 않아도 된다.
 const yourProduct:Partial<Product>={id:1};      // 속성을 부분적으로 지정해도 되며
 const ourProduct:Partial<Product>={id:1, name:'clothes', price:30000, brand:'musinsa', stock:30};   // 모든 속성을 지정해도 된다.
+
+// Partial은 이것과 똑같은 효과를 지닌다.
+// interface UpdateProduct{
+//     id?: number;
+//     name?: string;
+//     price?: number;
+//     brand?: string;
+//     stock?: number;
+// }
+
+type UpdateProduct = Partial<Product>
+// 3. 특정 상품정보를 업데이트(갱신)하는 함수
+function updateProductItem(productItem: Partial<Product>) {
+    
+}
+
+// 4. 유틸리티 타입 구현하기 - Partial
+interface UserProfile {
+    username: string;
+    email:string;
+    profilePhotoUrl: string;
+}
+// interface UserProfileUpdate{ 인터페이스에서 옵셔널 기능만 추가한 상태 파셜과 비슷하다.
+//     username?: string;
+//     email?:string;
+//     profilePhotoUrl?: string;
+// }
+
+// 첫번째 방법
+type UserProfileUpdate1 = {
+    username?: UserProfile['username'];
+    email?: UserProfile['email'];
+    profilePhonoUrl?: UserProfile['profilePhotoUrl'];
+}
+
+// 두번째 방법
+type UserProfileUpdate2 = {
+    [p in 'username' | 'email' | 'profilePhothUrl']?: UserProfile[p]    // for .. in 반복문이라고 보면 된다.
+}
+type UserProfileKeys = keyof UserProfile;
+
+// 세번째 방법(두번째 방법을 줄여놓은 것이다.)
+type UserProfileUpdate3 = {
+    [p in keyof UserProfile]?: UserProfile[p]
+}
+
+// 네번째 방법(이건 모든 인터페이스 타입을 partial을 사용하여 검사해주는 것이다.)
+type Subset<T> = {
+    [p in keyof T]?: T[p]
+}
+
