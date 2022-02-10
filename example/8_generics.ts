fb218fc61728dcb4770bd868e58aa521fc33dfc0
ABlued <53801395+ABlued@users.noreply.github.com>
2022-02-10 22:18:25 +0900

    제네릭 코드 설명 추가

--- example/8_generics.ts
+++ example/8_generics.ts
@@ -1,3 +1,8 @@
+/**
+ * 제네릭(generic)은 타입 정보가 동적으로 결정되는 타입이다. 제네릭을 통해 같은 규칙을 여러 타입에 적용할 수 있기 때문에
+ * 타입 코드를 작성할 때 발생할 수 있는 중복 코드를 제거할 수 있다.
+ */
+
 function getNumber(value: number) {
   return value;
 }
